54fed46225de2f4b88e82318e9b504f9ba57104c
Toghrul Mukhtarov <137985975+tommy-typos@users.noreply.github.com>
2024-08-17 13:51:59 +0200

    commit 1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
+import "@styles/tailwind-colors.scss";
+import "@styles/globals.scss";
+import "@styles/calendar.scss";
+import "@styles/calendar-colors.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
